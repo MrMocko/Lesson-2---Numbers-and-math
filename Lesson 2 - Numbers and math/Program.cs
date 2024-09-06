69303b8f3dec274fd54dd4c14b3710d7d5481668
Andre Mocko <339723967@GoTVDSB.ca>
2024-09-06 09:39:28 -0400

    Add project files.

---
+++ Lesson 2 - Numbers and math/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson_2___Numbers_and_math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I will now count my chickens");
+            Console.WriteLine("Hens " + (10 + 5 * 2));
+            Console.WriteLine("Roosters " + (25 + 30 / 6));
+            Console.WriteLine("six" + 6);
+            Console.WriteLine("6 + 6");
+            Console.WriteLine("6" + 6);
+            Console.WriteLine(6 + 6);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Now i will count the eggs:");
+            Console.WriteLine(11.0 / 2);
+            Console.WriteLine("Is it true that 3 + 2 < 5 - 7?");
+            Console.WriteLine(3 + 2 < 5 - 7);
+            Console.WriteLine("What is 3 + 2 " + (3 + 2));
+
+        }
+    }
+}
