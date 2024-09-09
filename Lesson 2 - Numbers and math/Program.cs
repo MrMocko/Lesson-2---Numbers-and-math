72cfbd4c721ba82867dc1190481542b4a828c5e5
Andre Mocko <339723967@GoTVDSB.ca>
2024-09-09 09:34:59 -0400

    Done

--- Lesson 2 - Numbers and math/Program.cs
+++ Lesson 2 - Numbers and math/Program.cs
@@ -22,6 +22,16 @@
             Console.WriteLine("Is it true that 3 + 2 < 5 - 7?");
             Console.WriteLine(3 + 2 < 5 - 7);
             Console.WriteLine("What is 3 + 2 " + (3 + 2));
+            Console.WriteLine("What is 5 - 7? " + (5 - 7));
+            Console.WriteLine("Oh that's why it's " + (3 + 2 < 5 - 7));
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("This fine and dandy day i worked 8 hours,let see how much i made today." + (21 * 8). ToString("C"));
+            Console.WriteLine("I will see how much each egg costs " + (6.0 / 12). ToString("C"));
+            Console.WriteLine("I bought a Billy mask for 69.99, lets see how much it costs after tax. " + (69.99 * 1.13). ToString("C"));
+            Console.WriteLine("Emmett is 5'6. Lets see how tall he is in centimeters " + (2.54 * (5 * 12 + 6)));
 
         }
     }
